3a1dcf2235a2cf4c068db409e71a0b34a67caf58
T.J. Daddio <dev@tjdaddio.com>
2021-06-07 16:28:40 -0400

    Adjust camera sensitivity to normal range

--- src/camera.rs
+++ src/camera.rs
@@ -31,7 +31,7 @@ impl FreeCamera {
             znear: 0.1,
             zfar: 100.0,
             speed,
-            sensitivity: 7.0,
+            sensitivity: 1.0 / 7.0,
         }
     }
 
@@ -67,6 +67,7 @@ impl Camera for FreeCamera {
     fn update(&mut self, inputs: &input::Inputs) {
         let speed = self.speed * inputs.delta_time;
         let (yaw_delta, pitch_delta) = inputs.mouse_delta();
+        println!("{:?}, {:?}", yaw_delta, pitch_delta);
         self.yaw += yaw_delta as f32 * self.sensitivity;
         self.yaw %= 360.0;
         self.pitch += pitch_delta as f32 * self.sensitivity;
